22afb2abfafeefe9d12fc4b43350efe7905ebb43
Clément Cardonnel <clement.cardonnel@me.com>
2025-03-26 09:03:24 +0100

    Made code safer

--- allowed-origin.js
+++ allowed-origin.js
@@ -1,5 +1,10 @@
 export const allowedOrigin = (event) => {
     const allowedOrigins = ["https://web.leafty.app", "https://www.leafty.app"];
+
+    if (event == null) {
+        return allowedOrigins[0];
+    }
+
     const origin = event.headers?.Origin || event.headers?.origin;
 
     const responseOrigin = allowedOrigins.includes(origin) ? origin : "https://web.leafty.app";
