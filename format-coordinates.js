714269f0c84055faf8ea36f3d189c9ca2624f3a9
Clément Cardonnel <clement.cardonnel@me.com>
2025-04-10 18:39:39 +0200

    PostGIS compatibility

--- format-coordinates.js
+++ format-coordinates.js
@@ -1,5 +1,13 @@
 /**
  * Convert coordinates to an object with lat and lon keys
+ * 
+ * In PostGIS, coordinates are represented as an array of [lon, lat].
+ * So if an array is passed, it is assumed to be in the format [lon, lat].
+ * **Do not** pass a [lat lon] array.
+ * 
+ * Learn more: https://postgis.net/documentation/tips/lon-lat-or-lat-lon/
+ * 
+ * @returns {Object} An object with lat and lon keys
 */
 export function formatCoordinates(coordinates) {
     if (!coordinates) {
@@ -10,15 +18,15 @@ export function formatCoordinates(coordinates) {
     if (typeof coordinates === 'object' && coordinates.type === 'Point' && 'coordinates' in coordinates) {
         const coordinateArray = coordinates.coordinates;
         return {
-            lat: coordinateArray[0],
-            lon: coordinateArray[1]
+            lat: coordinateArray[1],
+            lon: coordinateArray[0]
         };
     }
 
     if (Array.isArray(coordinates) && coordinates.length === 2) {
         return {
-            lat: coordinates[0],
-            lon: coordinates[1]
+            lat: coordinates[1],
+            lon: coordinates[0]
         };
     } else if ('x' in coordinates && 'y' in coordinates) {
         return {
